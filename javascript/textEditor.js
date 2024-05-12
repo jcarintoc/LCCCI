5e1eb2d1060d69ef8c82bbc19a8ce6baebbcbdd5
jcarintoc <154625365+jcarintoc@users.noreply.github.com>
2024-05-13 02:49:57 +0800

    Add files via upload

---
+++ javascript/textEditor.js
@@ -0,0 +1,8 @@
+
+tinymce.init({
+  selector: 'textarea#announcementContent',
+  plugins: 'lists link',
+  toolbar: 'undo redo | bold italic underline strikethrough | bullist numlist | link',
+  menubar: false,
+  height: 200
+});
